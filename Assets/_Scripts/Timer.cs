e62d28e4b3170da4dd6f886f3a512eb929ce2ced
Taylor-Henson <551726@chrome.escg.ac.uk>
2025-05-10 16:17:10 +0100

    created the timer home

---
+++ Assets/_Scripts/Timer.cs
@@ -0,0 +1,38 @@
+using System.Threading;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [Header("Timer")]
+    public TextMeshProUGUI timerText;
+    public float time;
+    public float maxTime;
+    public bool playing = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Starts the timer
+        time = maxTime;
+        timerText.text = time + "/" + maxTime;
+        playing = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time > 0 && playing)
+        {   
+            // Counts down and displays
+            time -= Time.deltaTime;
+            float roundedTime = Mathf.Round(time * 100f) / 100f;
+            timerText.text = roundedTime + "/" + maxTime;
+        }
+        else
+        {
+            // Game Over
+            playing = false;
+        }
+    }
+}
