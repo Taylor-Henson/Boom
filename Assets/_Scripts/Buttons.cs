cf84965689f8b0a4de10b454a3b0fffa962ef129
Taylor Henson <551726@escg.ac.uk>
2025-05-13 12:07:05 +0100

    added buttons and lighting fix

---
+++ Assets/_Scripts/Buttons.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    [Header("Arrays")]
+    public GameObject[] menus; 
+
+    public void LoadScene(int sceneNumber)
+    {
+        // Loads scene based off of integer chosen on the button
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+    public void QuitGame()
+    {
+        // Quits build
+        Application.Quit();
+    }
+
+    public void LoadMenu(int menuNumber)
+    {
+        // Sets every object in the array to disabled
+        foreach (GameObject menu in menus)
+        {
+            menu.SetActive(false);
+        }
+
+        // Enables a menu from an array based off of the integer chosen on the button
+        menus[menuNumber].gameObject.SetActive(true);
+    }
+}
